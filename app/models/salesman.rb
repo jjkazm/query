6c67b00173878aad3127c6ed03624f237f3e5d50
jjkazm <jjkazm@gmail.com>
2019-03-14 15:25:19 +0100

    Seed database

--- app/models/salesman.rb
+++ app/models/salesman.rb
@@ -1,4 +1,4 @@
 class Salesman < ApplicationRecord
   has_many :customers
-  has_many :orders through: :customers
+  has_many :orders, through: :customers
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do |n|
+  Salesman.create(name: Faker::Name.unique.name, city: Faker::WorldCup.city, commission: rand(12..15)/100)
+end
+
+8.times do |n|
+  Customer.create(cust_name: Faker::Name.unique.name, city: Faker::WorldCup.city, grade: rand(1..4) * 100, salesman_id: rand(1..Salesman.count))
+end
+
+20.times do |n|
+  Order.create(purch_amt: rand(75..2480), ord_date: rand(4.weeks.ago..2.days.ago), customer_id: rand(1..Customer.count))
+end
